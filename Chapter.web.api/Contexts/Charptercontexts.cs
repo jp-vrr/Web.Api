f3108c765953f4ef773ffbc134355a2b5b343958
João Pedro Vieira <jp.rodrigues.vr@gmail.com>
2022-02-18 17:33:18 -0300

    Adicionar arquivos de projeto.

---
+++ Chapter.web.api/Contexts/Charptercontexts.cs
@@ -0,0 +1,27 @@
+﻿using charpter.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace charpter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = CLIENTE-PC\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }// dbsetrepresenta as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
---
+++ Chapter.web.api/Controllers/ValuesController.cs
@@ -0,0 +1,103 @@
+﻿using charpter.models;
+using charpter.Repositors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace charpter.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivrosController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (System.Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorID(int id)
+        {
+            try
+            {
+                Livro livroProcurado = _livroRepository.BuscarPorID(id);
+
+                if (livroProcurado == null)
+                {
+                    return NotFound();
+                }
+                return Ok(livroProcurado);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult atualizar(int id, Livro livros)
+        {
+            try
+            {
+                _livroRepository.atualizar(id, livros);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+
+            }
+        }
+
+    }
+
+}
