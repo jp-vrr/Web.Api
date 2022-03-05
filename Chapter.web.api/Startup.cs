1af034d0b4c201fbcff7d0a1c833de5b64fdbb9b
jp-vrr <92949616+jp-vrr@users.noreply.github.com>
2022-03-05 19:18:42 -0300

    Add files via upload

--- Chapter.web.api/Startup.cs
+++ Chapter.web.api/Startup.cs
@@ -1,18 +1,20 @@
-using Charpter.Web.Api.Contexts;
-using Charpter.Web.Api.Repositors;
+using Chapter.web.api.interfaces;
+using Chapter.web.api.Repository;
+using charpter.Contexts;
+using charpter.Repositors;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Charpter.Web.Api
+namespace Chapter.web.api
 {
     public class Startup
     {
@@ -22,20 +24,54 @@ namespace Charpter.Web.Api
         }
 
         public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            // se não existir uma instância na memória da aplicação, cria um novo
+
+            services.AddCors(option =>
+             {
+                 option.AddPolicy("CorsPolicy",
+                 builder =>
+                 {
+                     builder.WithOrigins("http://localhost:5001")
+                     .AllowAnyHeader()
+                     .AllowAnyMethod();
+                 });
+             }); 
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "Charpter.WebApi" });
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Version = "v1", Title = "Charpter.Web.api" });
             });
-            services.AddScoped<Charptercontexts, Charptercontexts>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("Charpter-chave-autentication")),
+                    ClockSkew = TimeSpan.FromMinutes(60),
+                    ValidIssuer = "Chapter.web.api",
+                    ValidAudience = "Chapter.web.api"
+
+                };
+            });
+
+            services.AddScoped<ChapterContext, ChapterContext>();
             services.AddTransient<LivroRepository, LivroRepository>();
+            services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+            
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,20 +81,27 @@ namespace Charpter.Web.Api
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseSwagger();
+
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/Swagger/v1/swagger.json", "Chapter.WebApi");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Chapter.ApiWeb");
                 c.RoutePrefix = String.Empty;
             });
 
             app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
